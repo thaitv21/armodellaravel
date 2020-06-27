13a0196e67a8d531e5a72b73eef11e529be8c71a
Thân Thái <thaitv210@gmail.com>
2020-06-27 23:04:27 +0700

    s3

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use File;
+use Aws\S3\S3Client;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -15,24 +16,16 @@ class FileController extends \App\Http\Controllers\Controller
     protected $storage;
 
     public function __construct() {
-        #"gs://armodel-9459b.appspot.com"
-        $path = __DIR__ . '/firebase.json';
-        $serviceAccount = ServiceAccount::fromJsonFile($path); // đường dẫn của file json ta vừa tải phía trên
-        $this->storage = (new Factory())->withServiceAccount($serviceAccount)
-            ->createStorage();
+        $this->storage = Storage::disk('s3');
     }
 
     public function show()
     {
         $files = [];
-        foreach ($this->storage->getBucket()->objects() as $file) {
-            $name = $file->info()['name'];
-            array_push($files, ['name' => $name]);
+        foreach ($this->storage->allFiles("/models") as $file) {
+            array_push($files, ['name' => basename($file)]);
         }
-//        $disk = Storage::disk('local');
-//        foreach ($disk->allFiles("/models") as $file) {
-//            array_push($files, ['name' => basename($file)]);
-//        }
+        asort($files);
         return view('index', ['files' => $files]);
     }
 
@@ -50,17 +43,18 @@ class FileController extends \App\Http\Controllers\Controller
         if ($request->hasFile('model')) {
             $file = $request->model;
             $name = $file->getClientOriginalName();
-            $this->storage->getBucket()->upload(base64_encode(file_get_contents($file->path())), ["name" => $name]);
+            $request->file('model')->storeAs('models', $name, 's3');
         }
         return redirect('/index');
     }
 
     public function download($filename) {
-        $file = $this->storage->getBucket()->object($filename);
-        if (!$file->exists() || pathinfo($file->info()['name'])['extension'] != 'fbx') {
-            return view('error', ["err" => "Oops! $filename is not exists"]);
+        $path = 'models/'.$filename;
+        if (!$this->storage->exists($path)) {
+            return view('error', ["err" => "Oops! $filename isn't exists..."]);
         }
-        $expires = new \DateTimeImmutable('+60 minute');
-        return redirect($file->signedUrl($expires));
+        $url = Storage::disk('s3')->temporaryUrl($path, Carbon::now()->addMinutes(30));
+        return redirect($url);
+
     }
 }
